da7a91351f97ddb40d581d8854cf6364bc1081bd
Connor Huffine <chuffine@gmail.com>
2017-01-12 23:09:08 -0500

    Add RPi.GPIO as a requirement

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_filamentreload"
 plugin_name = "Octoprint-FilamentReload"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "1.0.0"
+plugin_version = "1.0.1"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/kontakt/Octoprint-Filament-Reloaded"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
+plugin_requires = ['RPi.GPIO']
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point

71bb3bbd3c5c985aaa3ba2a13ab2a29b23e0e79a
Adrià Cereto i Massagué <ssorgatem@gmail.com>
2020-09-26 12:50:19 +0200

    Revert "Merge branch 'simon-adriaanse-master' into master"
    This reverts commit 681bc9a0407e4e5ac2e1822f40f44f157f9ab8c3, reversing
    changes made to 4c9879cdd7f87fc6340ab2cfafdc5772da9d52cc.

--- octoprint_filamentreload/__init__.py
+++ octoprint_filamentreload/__init__.py
@@ -44,10 +44,6 @@ class FilamentReloadedPlugin(octoprint.plugin.StartupPlugin,
         return int(self._settings.get(["mode"]))
 
     @property
-    def pullup(self):
-        return int(self._settings.get(["pullup"]))
-
-    @property
     def no_filament_gcode(self):
         return str(self._settings.get(["no_filament_gcode"])).splitlines()
 
@@ -72,15 +68,9 @@ class FilamentReloadedPlugin(octoprint.plugin.StartupPlugin,
             else:
                 self._logger.info("Using BCM Mode")
                 GPIO.setmode(GPIO.BCM)
-
-            self._logger.info("Filament Sensor active on GPIO Pin [%s]"%self.pin)
-
-            if self.pullup == 0:
-                GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-                self._logger.info("Filament Sensor Pin uses pullup")
-            else:
-                GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-                self._logger.info("Filament Sensor Pin uses pulldown")
+            self._logger.info(
+                "Filament Sensor active on GPIO Pin [%s]" % self.pin)
+            GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         else:
             self._logger.info(
                 "Pin not configured, won't work unless configured!")

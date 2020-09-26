5c226e94bb33518d6ed2930d359bd1292a503502
Adrià Cereto i Massagué <ssorgatem@gmail.com>
2020-09-26 11:42:10 +0200

    revert change to setup.py

--- setup.py
+++ setup.py
@@ -33,8 +33,7 @@ plugin_url = "https://github.com/ssorgatem/Octoprint-Filament-Reloaded"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-#plugin_requires = ['RPi.GPIO']
-plugin_requires = []
+plugin_requires = ['RPi.GPIO']
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point

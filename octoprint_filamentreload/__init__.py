ffab2927a89aa4983a36eb082eefb58a3422f466
Adrià Cereto i Massagué <ssorgatem@gmail.com>
2020-09-26 13:37:55 +0200

    trying to debug some pesky bug

--- octoprint_filamentreload/__init__.py
+++ octoprint_filamentreload/__init__.py
@@ -89,6 +89,7 @@ class FilamentReloadedPlugin(octoprint.plugin.StartupPlugin,
 
     @property
     def pullup(self):
+        return 0#Let's try to debug this
         return int(self._settings.get(["pullup"] or 0))
 
     @property
